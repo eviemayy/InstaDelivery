ceff987447dedd2566d5976077b0016761dcd572
Mary May <mayma@oregonstate.edu>
2020-11-13 14:15:06 -0800

    added inserts to step 4 file

--- sql/step4.sql
+++ sql/step4.sql
@@ -1,4 +1,4 @@
--- CREATE
+-- A) DATA DEFINITION QUERIES
 
 CREATE TABLE customers (
     customerID INT AUTO_INCREMENT NOT NULL,
@@ -58,9 +58,67 @@ CREATE TABLE products_orders (
     FOREIGN KEY (orderID) REFERENCES orders(orderID)
 ) ENGINE=InnoDB
 
-
 -- Include the CASCADE where necessary
 
--- READ
--- UPDATE
--- DELETE
+-- B) SAMPLE DATA
+
+-- sample customers
+
+INSERT INTO customers (firstName, lastName, email, password, address)
+VALUES ('Harry', 'Potter', 'potterh@hogwarts.com', 'dumbledor1234', '608 NW Folk St');
+
+INSERT INTO customers (firstName, lastName, email, password, address)
+VALUES ('Ron', 'Weasley', 'weasleyr@hogwarts.com', 'ilovehermoine8', '244 Magic Ln');
+
+INSERT INTO customers (firstName, lastName, email, password, address)
+VALUES ('Hermione', 'Granger', 'grangerh@hogwarts.com', 'HgaBc749', '710 Monroe Ave');
+
+-- sample products
+
+INSERT INTO products (name, description, price, quantity)
+VALUES ('Apple', 'Fresh honeycrisp apples from golden orchard farms. Non GMO.',
+2.99, 800);
+
+INSERT INTO products (name, description, price, quantity)
+VALUES ('Banana', 'Non organic yellow banana.', 0.69, 1000);
+
+INSERT INTO products (name, description, price, quantity)
+VALUES ('Kale', 'Organic, common curly kale.', 2.00, 500);
+
+INSERT INTO products (name, description, price, quantity)
+VALUES ('Potato', 'Yukon gold potato from Idaho.', 2.00, 1000);
+
+INSERT INTO products (name, description, price, quantity)
+VALUES ('Celery', 'Packages celery. Comes in a pack of approx. 10 stalks.', 2.50, 300);
+
+INSERT INTO products (name, description, price, quantity)
+VALUES ('Tomato', 'Roma tomatoes. Non GMO.', 0.69, 900);
+
+INSERT INTO products (name, description, price, quantity)
+VALUES (`Dave's Killer Bread`, `Dave's Killer Bread Good Seed Thin Sliced Organic.`, 5.49, 300);
+
+INSERT INTO products (name, description, price, quantity)
+VALUES ('Eggs', 'Cage free brown eggs.', 2.49, 300);
+
+INSERT INTO products (name, description, price, quantity)
+VALUES ('Milk', '2% Reduced Fat Milk.', 2.00, 550);
+
+INSERT INTO products (name, description, price, quantity)
+VALUES ('Greek Yogurt', 'Greek Gods Plain Traditional Greek Style Yogurt.', 3.79, 500);
+
+INSERT INTO products (name, description, price, quantity)
+VALUES ('Ice Cream', 'Tillamook cookie dough ice cream. 1.75 qt.', 5.99, 100);
+
+INSERT INTO products (name, description, price, quantity)
+VALUES ('Cheddar Cheese', 'Tillamook sharp cheddar cheese.', 3.79, 750);
+
+-- sample deliverers
+
+INSERT INTO deliverers (firstName, lastName)
+VALUES ('Draco', 'Malfoy');
+
+INSERT INTO deliverers (firstName, lastName)
+VALUES ('Rubeus', 'Hagrid');
+
+INSERT INTO deliverers (firstName, lastName)
+VALUES ('Severus', 'Snape');
